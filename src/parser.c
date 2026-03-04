11d3516bea122af9ce856a2b82e34b14cbd34193
MM Yap <mryap2@up.edu.ph>
2026-03-05 03:48:27 +0800

    implement load_processes function in parser.c

---
+++ src/parser.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include "parser.h"
+
+int load_processes(const char *filename, Process processes[], int max_processes) {
+
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL) {
+        printf("Error: Could not open workload file.\n");
+        return -1;
+    }
+
+    int count = 0;
+
+    while (count < max_processes) {
+
+        char line[128];
+
+        if (fgets(line, sizeof(line), file) == NULL)
+            break;
+
+        // skip comments
+        if (line[0] == '#')
+            continue;
+
+        Process p;
+
+        if (sscanf(line, "%s %d %d",
+                   p.pid,
+                   &p.arrival_time,
+                   &p.burst_time) == 3) {
+
+            p.remaining_time = p.burst_time;
+            p.start_time = -1;
+            p.finish_time = 0;
+
+            p.waiting_time = 0;
+            p.turnaround_time = 0;
+            p.response_time = 0;
+
+            p.priority = 0;
+            p.time_in_queue = 0;
+
+            processes[count] = p;
+            count++;
+        }
+    }
+
+    fclose(file);
+
+    return count;
+}
