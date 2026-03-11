5191249c7ebb52357b191e7e85a6aa63ebd46c54
Christel Hope Ong <aristelhope@gmail.com>
2026-03-11 19:27:19 +0800

    fix array order issue in gantt.c
    instead of printing in array order, print sorted by start time to display the correct gantt chart for sjf algo

--- src/gantt.c
+++ src/gantt.c
@@ -6,13 +6,27 @@ void print_gantt_chart(Process *processes, int n) {
     
     printf("\nGantt Chart:\n");
 
+    for (int i = 0; i < n - 1; i++) { // sort by start time
+        for (int j = i + 1; j < n; j++) {
+
+            if (processes[j].start_time < processes[i].start_time) {
+
+                Process temp = processes[i];
+                processes[i] = processes[j];
+                processes[j] = temp;
+            }
+        }
+    }
+
     for (int i = 0; i < n; i++) {
         printf("| %s ", processes[i].pid);
     }
+
     printf("|\n");
 
     for (int i = 0; i < n; i++) {
         printf("%d   ", processes[i].start_time);
     }
+
     printf("%d\n\n", processes[n-1].finish_time);
 }
